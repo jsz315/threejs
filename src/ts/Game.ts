cc219d1467d778ff987d203ee28549ac9a0fa31c
jsz315 <jsz_thing@126.com>
2019-08-07 11:47:59 +0800

    引用模型loader工具

--- src/ts/Game.ts
+++ src/ts/Game.ts
@@ -1,23 +1,24 @@
 import * as THREE from 'three'
 let OrbitControls = require('three-orbit-controls')(THREE);
 
+import { TDSLoader } from 'three/examples/jsm/loaders/TDSLoader';
 // import {OrbitControls} from 'three-orbitcontrols-ts'
 // import {OrbitControls} from 'three/examples/js/controls/OrbitControls';
 
-export default class Game{
+export default class Game {
     scene: THREE.Scene;
     camera: THREE.PerspectiveCamera;
     renderer: THREE.WebGLRenderer;
     ambient: THREE.AmbientLight;
     directional: THREE.DirectionalLight;
 
-    constructor(){
+    constructor() {
         this.scene = new THREE.Scene();
-        this.camera = new THREE.PerspectiveCamera(75, window.innerWidth /window.innerHeight, 0.1, 900);
+        this.camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 900);
         this.renderer = new THREE.WebGLRenderer();
     }
 
-    setup(){
+    setup() {
         let geometry = new THREE.BoxGeometry();
         let material = new THREE.MeshNormalMaterial();
         let cube = new THREE.Mesh(geometry, material);
@@ -36,35 +37,60 @@ export default class Game{
         document.body.appendChild(this.renderer.domElement);
         new OrbitControls(this.camera, this.renderer.domElement);
 
-
         this.ambient = ambient;
         this.directional = directional;
 
-        this.loadObj();
+        this.loadJSON();
+        this.load3DS();
         this.animate();
     }
 
-    changeColor(param: any){
-        if(param.name == "Background"){
+    changeColor(param: any) {
+        if (param.name == "Background") {
             this.renderer.setClearColor(new THREE.Color(param.data));
         }
-        else if(param.name == "AmbientLight"){
+        else if (param.name == "AmbientLight") {
             this.ambient.color = new THREE.Color(param.data);
         }
-        else if(param.name == "DirectionalLight"){
+        else if (param.name == "DirectionalLight") {
             this.directional.color = new THREE.Color(param.data)
         }
     }
 
-    loadObj(){
+    loadJSON() {
         let loader = new THREE.ObjectLoader();
-        loader.load("/obj/teapot-claraio.json", (obj: THREE.Object3D)=>{
+        loader.load("/obj/teapot-claraio.json", (obj: any) => {
+            obj.material = new THREE.MeshNormalMaterial();
+            obj.position.set(0, 4, 0);
             this.scene.add(obj);
         })
     }
 
-    animate(){
-        requestAnimationFrame(()=>{
+    load3DS() {
+        let loader = new TDSLoader();
+        // var resourcePath = '/obj/portalgun/textures/';
+        // var url = '/obj/portalgun/portalgun.3ds';
+
+        var resourcePath = '/obj/house/';
+        var url = '/obj/house/QQ.3DS';
+        loader.setResourcePath(resourcePath);
+        loader.load(url, (object) => {
+            console.log(object)
+            object.traverse(function (child: any) {
+                if (child.isMesh) {
+                    console.log(child);
+                    // child.material.normalMap = normal;
+                    // child.material = new THREE.MeshNormalMaterial();
+                    child.scale.set(0.004, 0.004, 0.004);
+                }
+
+            });
+            this.scene.add(object);
+        })
+    }
+
+    animate() {
+        requestAnimationFrame(() => {
             this.animate();
         });
         this.renderer.render(this.scene, this.camera);
